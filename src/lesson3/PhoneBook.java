d7c296b9a8e019cad2d56701fcfd34afcee72d7d
ZhenisT <corpsolut@mail.ru>
2019-06-21 12:36:14 +0600

    ДЗ 3

--- src/lesson3/PhoneBook.java
+++ src/lesson3/PhoneBook.java
@@ -4,25 +4,25 @@ package lesson3;
 import java.util.*;
 
 public class PhoneBook {
-    private Map<String, LinkedList<String>> map = new HashMap<>();
+    private Map<String, LinkedList<String>> pb = new HashMap<>();
 
     public PhoneBook() {
-        this.map = map;
+        this.pb = pb;
     }
 
     public void add(String fam, String num) {
         LinkedList<String> numb = new LinkedList<>();
-        if (map.containsKey(fam)) {
-            numb = map.get(fam);
+        if (pb.containsKey(fam)) {
+            numb = pb.get(fam);
         }
 
         numb.add(num);
-        map.put(fam, numb);
+        pb.put(fam, numb);
     }
 
     public LinkedList<String> get(String fam) {
         System.out.print(fam + ": ");
-        return map.get(fam);
+        return pb.get(fam);
     }
 
 }
