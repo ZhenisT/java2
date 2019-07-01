bb6fd0b6a483303a628732f230c5c8063a3eb88f
ZhenisT <corpsolut@mail.ru>
2019-07-01 11:24:53 +0600

    Refactored

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -12,42 +12,29 @@ public class Main {
 
     public static void fullSizeArr(int size) {
         long a = System.currentTimeMillis();
-        float[] fullSizeArr = new float[size];
-        for (int i = 0; i < size; i++) {
-            fullSizeArr[i] = 1;
-        }
-        for (int i = 0; i < size; i++) {
-            fullSizeArr[i] = (float) (fullSizeArr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
+        float[] fullSizeArr = floatArray(size);
+        productMath(size, fullSizeArr);
         System.out.println("Первый просто бежит по массиву и вычисляет значения");
         System.out.println("Результат: "+(System.currentTimeMillis() - a));
     }
-
 
     public static void halfSizeArr(int h, int size) {
         long a = System.currentTimeMillis();
         float[] arr1 = new float[h];
         float[] arr2 = new float[h];
 
-        float[] fullSizeArr = new float[size];
-        for (int i = 0; i < size; i++) {
-            fullSizeArr[i] = 1;
-        }
+        float[] fullSizeArr = floatArray(size);
 
         System.arraycopy(fullSizeArr, 0, arr1, 0, h);
         System.arraycopy(fullSizeArr, h, arr2, 0, h);
 
 
         Thread t1 = new Thread(() -> {
-            for (int i = 0; i < h; i++) {
-                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
+            productMath(h, arr1);
         });
 
         Thread t2 = new Thread(() -> {
-            for (int i = 0; i < h; i++) {
-                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
+            productMath(h, arr2);
         });
 
         t1.start();
@@ -58,5 +45,22 @@ public class Main {
         System.out.println("Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один");
         System.out.println("Результат: "+(System.currentTimeMillis() - a));
     }
+
+    private static float[] floatArray(int size) {
+        float[] fullSizeArr = new float[size];
+        for (int i = 0; i < size; i++) {
+            fullSizeArr[i] = 1;
+        }
+        return fullSizeArr;
+    }
+
+    private static void productMath(int size, float[] fullSizeArr) {
+        for (int i = 0; i < size; i++) {
+            fullSizeArr[i] = (float) (fullSizeArr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+
+
+
 
 }
