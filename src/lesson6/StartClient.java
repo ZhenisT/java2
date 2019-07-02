d8ef06ecbbb7ca067d107a01947fdf83044717a8
ZhenisT <corpsolut@mail.ru>
2019-07-02 16:40:51 +0600

    ДЗ 6

---
+++ src/lesson6/StartClient.java
@@ -0,0 +1,9 @@
+package lesson6;
+
+import lesson6.Client.ClientChat;
+
+public class StartClient {
+    public static void main(String[] args) {
+        ClientChat clientChat = new ClientChat();
+    }
+}
---
+++ src/lesson6/StartServer.java
@@ -0,0 +1,11 @@
+package lesson6;
+
+import lesson6.Client.ClientChat;
+import lesson6.Server.EchoServer;
+
+public class StartServer {
+    public static void main(String[] args) {
+EchoServer startServer = new EchoServer();
+
+    }
+}
